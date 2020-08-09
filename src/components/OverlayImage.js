3ed7b7f907e0d728eb2a4925ae871136067e003b
alexbcberio <alexbcberio@gmail.com>
2020-08-09 19:21:16 +0200

    Show loading icon + press Escape to close

--- src/components/OverlayImage.js
+++ src/components/OverlayImage.js
@@ -5,6 +5,12 @@ Vue.component('OverlayImage', {
     "src",
     "title"
   ],
+  data: function() {
+    return {
+      visible: false,
+      imgPreloaded: false
+    };
+  },
   methods: {
     /*
      * adapted from https://davidwalsh.name/javascript-download
@@ -23,10 +29,45 @@ Vue.component('OverlayImage', {
 
       window.URL.revokeObjectURL(a.href);
       document.body.removeChild(a);
+
+      return a.href;
+    },
+    preloadImage: function() {
+      const img = new Image();
+      img.style.display = "none";
+      document.body.appendChild(img);
+
+      img.src = this.src;
+
+      img.onload = () => {
+        this.imgPreloaded = true;
+        document.body.removeChild(img);
+      }
+    },
+    kbdControls: function(e) {
+      if (this.visible) {
+        switch(e.code) {
+          case "Escape":
+            this.$emit("close");
+            break;
+        }
+      }
     }
   },
+  watch: {
+    "src": function() {
+      this.imgPreloaded = false;
+      this.preloadImage();
+    }
+  },
+  mounted() {
+    document.addEventListener("keydown", this.kbdControls);
+  },
+  beforeDestroy() {
+    document.removeEventListener("keydown", this.kbdControls);
+  },
   template: `
-  <transition enter-active-class="animate__animated animate__fadeIn" leave-active-class="animate__animated animate__fadeOut">
+  <transition enter-active-class="animate__animated animate__fadeIn" leave-active-class="animate__animated animate__fadeOut" @before-enter="visible = true" @after-leave="visible = false">
     <div class="overlay">
       <div class="overlay-header">
           <span></span>
@@ -34,7 +75,9 @@ Vue.component('OverlayImage', {
           <span class="close" @click="$emit('close')">&times;</span>
       </div>
       <div class="overlay-content">
-          <div class="image" :style="{backgroundImage: 'url(' + src + ')'}" @click="downloadImage" :title="$t('terms.iTunes-search.download-image.download')"></div>
+          <div class="image" :style="{backgroundImage: imgPreloaded ? 'url(' + src + ')' : ''}" @click="downloadImage" :title="$t('terms.iTunes-search.download-image.download')">
+            <loader v-if="!imgPreloaded" />
+          </div>
       </div>
     </div>
   </transition>
